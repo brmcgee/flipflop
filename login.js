53d3131d9274a8580b983e7d72169176bcd0bd9a
Brian McGee <92900596+brmcgee@users.noreply.github.com>
2024-11-01 19:33:59 -0400

    Add files via upload

--- login.js
+++ login.js
@@ -52,8 +52,8 @@ async function handleLoginClick(id) {
         }
         root.innerHTML = alertClient(client);
         loginCont.style.display = 'none';
-        setTimeout(closeAlert, 1500)
-        setTimeout(dashboard, 1500)
+        setTimeout(closeAlert, 500)
+        setTimeout(dashboard, 500)
     }
     else { 
         client.status = 'invalid';
@@ -64,8 +64,235 @@ async function handleLoginClick(id) {
     } 
 }
 function dashboard() {
-    root.innerHTML = `<h1 class="bg-dark text-light m-0">Welcome ${client.username}</h1>
-                        <img src='${client.avatar}' class='mx-auto rounded-circle bg-secondary' width="120" height="120" />`
+
+
+    root.innerHTML = `<h1 class="bg-dark m-0 mb-2">Welcome </h1>
+                 
+                        
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+<section class="blog-listing gray-bg">
+        <div class="container">
+            <div class="row align-items-start">
+               
+            
+
+
+
+
+
+ <div class="col-lg-8 m-15px-tb">
+                    <div class="row">
+                        
+                        
+                     
+                        <div class="col-sm-6">
+                            <div class="blog-grid">
+                                <div class="blog-img">
+                                    <div class="date">
+                                        <span>04</span>
+                                        <label>FEB</label>
+                                    </div>
+                                    <a href="#">
+                                        <img src="https://tse2.mm.bing.net/th?id=OIP.woLDWOAKygs7PJ8AL0u2VwHaEJ&pid=Api&P=0&h" title="" alt="" class="img-fluid">
+                                    </a>
+                                </div>
+                                <div class="blog-info">
+                                    <h5><a href="#">Prevent 75% of visitors from google analytics</a></h5>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                    <div class="btn-bar">
+                                        <a href="#" class="px-btn-arrow">
+                                            <span>Read More</span>
+                                            <i class="arrow"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="blog-grid">
+                                <div class="blog-img">
+                                    <div class="date">
+                                        <span>04</span>
+                                        <label>FEB</label>
+                                    </div>
+                                    <a href="#">
+                                        <img class="img-fluid" src="https://image.cnbcfm.com/api/v1/image/108029615-17255396102024-09-04t233804z_605513800_rc2vt9a21usv_rtrmadp_0_usa-economy.jpeg" title="" alt="">
+                                    </a>
+                                </div>
+                                <div class="blog-info">
+                                    <h5><a href="#">Prevent 75% of visitors from google analytics</a></h5>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                    <div class="btn-bar">
+                                        <a href="#" class="px-btn-arrow">
+                                            <span>Read More</span>
+                                            <i class="arrow"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <ul class="pagination justify-content-center">
+                                <li class="page-item disabled">
+                                    <a class="page-link" href="#" tabindex="-1"><i class="fas fa-chevron-left"></i></a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item active">
+                                    <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#"><i class="fas fa-chevron-right"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+                <div class="col-lg-4 m-15px-tb blog-aside">
+                    <!-- Author -->
+                    <div class="widget widget-author">
+                        <div class="widget-title">
+                            <h3>${client.username}</h3>
+                        </div>
+                        <div class="widget-body">
+                            <div class="media align-items-center">
+                                <div class="avatar">
+                                    <img src="${client.avatar}" title="" alt="" width="200" height="200" class="rounded-circle">
+                                </div>
+                                <div class="media-body">
+                                    <h6>Hello, I'm<br> ${client.username}</h6>
+                                </div>
+                            </div>
+                            <p></p>
+                        </div>
+                    </div>
+                    <!-- End Author -->
+
+                    <!-- Trending Post -->
+                    <div class="widget widget-post">
+                        <div class="widget-title">
+                            <h3>Trending Now</h3>
+                        </div>
+                        <div class="widget-body">
+
+                        </div>
+                    </div>
+                    <!-- End Trending Post -->
+
+                    <!-- Latest Post -->
+                    <div class="widget widget-latest-post">
+                        <div class="widget-title">
+                            <h3>Latest Post</h3>
+                        </div>
+                        <div class="widget-body">
+                            <div class="latest-post-aside media">
+                                <div class="lpa-left media-body">
+                                    <div class="lpa-title">
+                                        <h5><a href="#">Prevent 75% of visitors from google analytics</a></h5>
+                                    </div>
+                                    <div class="lpa-meta">
+                                        <a class="name" href="#">
+                                            Rachel Roth
+                                        </a>
+                                        <a class="date" href="#">
+                                            26 FEB 2020
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="lpa-right">
+                                    <a href="#">
+                                        <img src="https://ichef.bbci.co.uk/news/1024/cpsprodpb/BF9F/production/_108855094_gettyimages-1163654178.jpg.webp" class="img-fluid" title="" alt="">
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="latest-post-aside media">
+                                <div class="lpa-left media-body">
+                                    <div class="lpa-title">
+                                        <h5><a href="#">Prevent 75% of visitors from google analytics</a></h5>
+                                    </div>
+                                    <div class="lpa-meta">
+                                        <a class="name" href="#">
+                                            Rachel Roth
+                                        </a>
+                                        <a class="date" href="#">
+                                            26 FEB 2020
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="lpa-right">
+                                    <a href="#">
+                                        <img src="https://cdn.mos.cms.futurecdn.net/rfxSqdye9LWXXwRAwDpDNQ-650-80.jpg.webp" class="img-fluid" title="" alt="">
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="latest-post-aside media">
+                                <div class="lpa-left media-body">
+                                    <div class="lpa-title">
+                                        <h5><a href="#">Prevent 75% of visitors from google analytics</a></h5>
+                                    </div>
+                                    <div class="lpa-meta">
+                                        <a class="name" href="#">
+                                            Rachel Roth
+                                        </a>
+                                        <a class="date" href="#">
+                                            26 FEB 2020
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="lpa-right">
+                                    <a href="#">
+                                        <img class="img-fluid" src="https://images.hdqwalls.com/download/fortnite-battle-royale-hd-wz-1920x1080.jpg" title="" alt="">
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <!-- End Latest Post -->
+
+                    <!-- widget Tags -->
+                    <div class="widget widget-tags">
+                        <div class="widget-title">
+                            <h3>Latest Tags</h3>
+                        </div>
+                        <div class="widget-body">
+                            <div class="nav tag-cloud">
+                                <a href="#">Design</a>
+                                <a href="#">Development</a>
+                                <a href="#">Travel</a>
+                                <a href="#">Web Design</a>
+                                <a href="#">Marketing</a>
+                                <a href="#">Research</a>
+                                <a href="#">Managment</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End widget Tags -->
+                </div>
+            </div>
+        </div>
+    </section> 
+                        
+                        
+                        
+                        
+                        `;
+
+
+                        
 }
 function closeAlert() {
     document.querySelector('.login-alert').style.display = 'none';
